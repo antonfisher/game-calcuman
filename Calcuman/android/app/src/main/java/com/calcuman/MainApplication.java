3011d1a53870285f1e388c5805d74ccaf2878f87
Anton Fisher <a.fschr@gmail.com>
2016-11-21 01:45:55 -0800

    Fix naming trouble

--- Calcuman/android/app/src/main/java/com/calcuman/MainApplication.java
+++ Calcuman/android/app/src/main/java/com/calcuman/MainApplication.java
@@ -4,6 +4,9 @@ import android.app.Application;
 import android.util.Log;
 
 import com.facebook.react.ReactApplication;
+import com.oblador.vectoricons.VectorIconsPackage;
+import com.zmxv.RNSound.RNSoundPackage;
+import com.idehub.GoogleAnalyticsBridge.GoogleAnalyticsBridgePackage;
 import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
@@ -11,9 +14,6 @@ import com.facebook.react.shell.MainReactPackage;
 
 import java.util.Arrays;
 import java.util.List;
-
-import com.zmxv.RNSound.RNSoundPackage;
-import com.idehub.GoogleAnalyticsBridge.GoogleAnalyticsBridgePackage;
 
 public class MainApplication extends Application implements ReactApplication {
 
@@ -27,8 +27,9 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-          new RNSoundPackage(),
-          new GoogleAnalyticsBridgePackage()
+            new VectorIconsPackage(),
+            new RNSoundPackage(),
+            new GoogleAnalyticsBridgePackage()
       );
     }
   };
