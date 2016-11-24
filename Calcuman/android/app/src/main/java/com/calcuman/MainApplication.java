9f5537c32f8184d91285ebd9d5e1f0c61c4f4760
Anton Fisher <a.fschr@gmail.com>
2016-11-24 02:06:58 -0800

    Another upgrade >___<

--- Calcuman/android/app/src/main/java/com/calcuman/MainApplication.java
+++ Calcuman/android/app/src/main/java/com/calcuman/MainApplication.java
@@ -11,6 +11,7 @@ import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
+import com.facebook.soloader.SoLoader;
 
 import java.util.Arrays;
 import java.util.List;
@@ -27,15 +28,21 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-            new VectorIconsPackage(),
-            new RNSoundPackage(),
-            new GoogleAnalyticsBridgePackage()
+          new VectorIconsPackage(),
+          new RNSoundPackage(),
+          new GoogleAnalyticsBridgePackage()
       );
     }
   };
 
   @Override
   public ReactNativeHost getReactNativeHost() {
-      return mReactNativeHost;
+    return mReactNativeHost;
+  }
+
+  @Override
+  public void onCreate() {
+    super.onCreate();
+    SoLoader.init(this, /* native exopackage */ false);
   }
 }
